c244c63143dd0b9a7a5e46a7402053c3fefbb04a
SoniaMorones <soniamorones8@gmail.com>
2023-03-23 17:54:28 -0600

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Instancia de cuenta de banco
+        CuentaBanco cuentaBanco = new CuentaBanco("Luis Lopez", 1000);
+
+        //ACTIVIDAD
+        //Retirar 500
+        cuentaBanco.retiro(500);
+
+        //Depositar 5,000
+        cuentaBanco.deposito(5000);
+
+        //Retirar  2,000
+        cuentaBanco.retiro(2000);
+
+        //Imprime el nombre del propietario de la cuenta
+        System.out.println("Nombre proietario de cuenta :"+cuentaBanco.getPropietario());
+
+        //Imprime el saldo actual del propietario
+        System.out.println("Saldo actual: "+cuentaBanco.getSaldo());
+
+    }
+}
